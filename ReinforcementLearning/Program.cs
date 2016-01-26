f91da97b60fa42970ed33769bfdc53deb6098272
Ley789 <A.Lochmann@student.uibk.ac.at>
2016-01-26 20:33:54 +0100

    added output

--- ReinforcementLearning/Program.cs
+++ ReinforcementLearning/Program.cs
@@ -13,25 +13,37 @@ namespace ReinforcementLearning
             Dynamic dpIteration = new Dynamic();
             Dynamic dpEvalAndImprov = new Dynamic();
 
+            Console.WriteLine("We will go through the Policy Improvement iteration");
+            Console.ReadLine();
             Console.WriteLine("Our initialized value function per state.");
             dpIteration.PrintV();
-            Console.WriteLine("Now after Vlaue Iteration");
-            dpIteration.ValueIteration();
-            dpIteration.PrintV();
-            Console.WriteLine("Now after Policy evaluation");
+            Console.ReadLine();
+            Console.WriteLine("Now after Policy evaluation, 1000 Iterations and discount 0.8");
+            Console.ReadLine();
             dpEvalAndImprov.PolicyEvaluation(1000);
             dpEvalAndImprov.PrintV();
-
+            Console.ReadLine();
             Console.WriteLine("Now we improve our Policy");
-            Console.WriteLine("This is after Value Iteration");
-            dpIteration.PolicyImprovement();
-            dpIteration.PrintP();
-            Console.WriteLine("This is after Policy evaluation");
+            Console.ReadLine();
             dpEvalAndImprov.PolicyImprovement();
             dpEvalAndImprov.PrintP();
+            Console.ReadLine();
 
-      
-            Console.Read();
+            Console.WriteLine("In this case we find the best Policy after 1 iteration");
+            Console.ReadLine();
+            Console.WriteLine("We will go through the Policy Improvement iteration");
+            
+            Console.WriteLine("Now after Vlaue Iteration of the initialized value");
+            Console.ReadLine();
+            dpIteration.ValueIteration();
+            dpIteration.PrintV();
+            Console.ReadLine();
+            Console.WriteLine("Now we improve our Policy");
+            Console.ReadLine();
+            dpIteration.PolicyImprovement();
+            dpIteration.PrintP();
+
+            Console.ReadLine();
         }
     }
 }
